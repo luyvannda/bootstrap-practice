16ca23c52d18f229e46da9dc2dfa625266b3820f
luyvannda <philips.joe329@gmail.com>
2023-10-31 16:07:08 +0700

    practice using grid column

--- src/components/BootStrap.jsx
+++ src/components/BootStrap.jsx
@@ -1,23 +1,19 @@
 export default function Bootstrap() {
   return (
     <>
-      <div className="container pt-5">
-        <h1>Hello, Bootstrap and Vite!</h1>
-        <button className="btn btn-danger">Primary button</button>
-      </div>
+      <div className="container-md border">
 
-      <div className="row mt-3 mb-3">
-        <div className="col-sm-4 bg-black text-white text-center">Column 1</div>
-        <div className="col-sm-8 bg-primary text-white text-center">Column-2</div>
-      </div>
-
-      <div className="container mt-3">
-        <h1>Blockquotes</h1>
-        <p>The blockquote element is used to present content from another source:</p>
-        <blockquote className="blockquote">
-          <p>For 50 years, <abbr title="World Wide Fund for Nature">WWF</abbr>  has been protecting the future of nature. The world&apos; leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.</p>
-          <footer className="blockquote-footer">From WWF&apos; website</footer>
-        </blockquote>
+        <div className="row pb-3 text-center">
+          <div className="col-md-8">
+            <div className="p-3">col 1</div>
+            <div className="row">
+              <div className="col-md-4 p-3">col-md-4</div>
+              <div className="col-md-4 p-3">col-md-4</div>
+              <div className="col-md-4 p-3">col-md-4</div>
+            </div>
+          </div>
+          <div className="col-md-4">Col 2</div>
+        </div>
       </div>
     </>
   )
