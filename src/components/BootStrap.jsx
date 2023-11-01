bbc60cb27bf611dc86449523eb4c357c0bc8f7e2
luyvannda <philips.joe329@gmail.com>
2023-11-01 15:48:31 +0700

    learn to design table with bootstrap

--- src/components/BootStrap.jsx
+++ src/components/BootStrap.jsx
@@ -1,19 +1,70 @@
 export default function Bootstrap() {
   return (
     <>
-      <div className="container-md border">
+      <div className="container-md">
 
-        <div className="row pb-3 text-center">
-          <div className="col-md-8">
-            <div className="p-3">col 1</div>
-            <div className="row">
-              <div className="col-md-4 p-3">col-md-4</div>
-              <div className="col-md-4 p-3">col-md-4</div>
-              <div className="col-md-4 p-3">col-md-4</div>
-            </div>
-          </div>
-          <div className="col-md-4">Col 2</div>
-        </div>
+        <h3>This is a table</h3>
+        <p>This table was created using Bootstrap</p>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Products Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="table-primary">
+              <td>Aloe Vera Gel</td>
+              <td>35 USD</td>
+              <td>1 Unit</td>
+            </tr>
+
+            <tr className="table-info">
+              <td>Aloe Berry Nectar</td>
+              <td>35 USD</td>
+              <td>1 Unit</td>
+            </tr>
+
+            <tr className="table-success">
+              <td>Bee Propolis</td>
+              <td>40 USD</td>
+              <td>1 Bottle</td>
+            </tr>
+
+            <tr className="table-warning">
+              <td>Royal Jelly</td>
+              <td>40 USD</td>
+              <td>1 Bottle</td>
+            </tr>
+
+            <tr className="table-danger">
+              <td>MPD 2x Ultra</td>
+              <td>30 USD</td>
+              <td>1 Bottle</td>
+            </tr>
+            <tr className="table-secondary">
+              <td>Bright Tooth Gel</td>
+              <td>10 USD</td>
+              <td>1 Tube</td>
+            </tr>
+            <tr className="table-active">
+              <td>Forever Calcium</td>
+              <td>25 USD</td>
+              <td>1 Bottle</td>
+            </tr>
+            <tr className="table-light">
+              <td>Nature Min</td>
+              <td>25 USD</td>
+              <td>1 Bottle</td>
+            </tr>
+            <tr className="table-dark">
+              <td>Absorbent C</td>
+              <td>25 USD</td>
+              <td>1 Bottle</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   )
