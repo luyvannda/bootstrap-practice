b083233e2c8ae157a0e6fb41652f59c92ce5f65e
luyvannda <philips.joe329@gmail.com>
2023-11-03 10:56:43 +0700

    learn all about bootstrap button

--- src/components/BootStrap.jsx
+++ src/components/BootStrap.jsx
@@ -1,17 +1,41 @@
+
 export default function Bootstrap() {
   return (
     <>
-      <div className="container-md">
-        <div className="alert alert-success alert-dismissible fade show">
-          <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
-          <strong>Success!</strong> This alert box could indicate a successful or positive action.
-        </div>
-        <div className="alert alert-info alert-dismissible fade show">
-          <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
-          <strong>Info!</strong> This alert box could indicate a neutral informative change or action.
+      <div className="container-md pt-4">
+
+        <button className="btn btn-outline-primary mx-2">Primary</button>
+        <button className="btn btn-outline-secondary mx-2">Secondary</button>
+        <button className="btn btn-outline-info mx-2" >Info</button>
+        <button className="btn btn-outline-success mx-2 btn-lg">Success</button>
+        <button className="btn btn-outline-warning mx-2 btn-sm">Warning</button>
+
+        <div className="d-grid mt-2 gap-2">
+          <button className="btn btn-danger btn-block">Danger</button>
+          <button className="btn btn-dark btn-block">Dark</button>
+          <button className="btn btn-light btn-block">Light</button>
         </div>
 
+        <button className="btn btn-primary" type="button" disabled>
+          <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...
+        </button>
 
+        <button className="btn btn-info" type="button">
+          <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span> Loading...
+        </button>
+
+        <div className="btn-group">
+          <button type="button" className="btn btn-primary">Sony</button>
+          <button type="button" className="btn btn-primary">Samsung</button>
+          <button type="button" className="btn btn-warning">Apple</button>
+          <button type="button" className="btn btn-warning">Oppo</button>
+        </div>
+
+        <div className="btn-group-vertical">
+          <button type="button" className="btn btn-primary">Apple</button>
+          <button type="button" className="btn btn-primary">Samsung</button>
+          <button type="button" className="btn btn-primary">Sony</button>
+        </div>
 
       </div >
     </>
