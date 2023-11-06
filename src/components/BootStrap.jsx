b9b02b7c44cf5f4c2edd5de0124868e455469433
luyvannda <philips.joe329@gmail.com>
2023-11-06 16:12:42 +0700

    complete spinners practice

--- src/components/BootStrap.jsx
+++ src/components/BootStrap.jsx
@@ -3,47 +3,39 @@ export default function Bootstrap() {
   return (
     <>
       <div className="container pt-3">
-        <h1>Hello</h1>
-        <div className="progress mb-2" style={{ minHeight: "1.5rem" }}>
-          <div className="progress-bar" style={{ minWidth: "10%" }} >10%</div>
-        </div>
-
-        <div className="progress mb-2" style={{ minHeight: "1.5rem" }}>
-          <div className="progress-bar progress-bar-striped bg-success" style={{ minWidth: "20%" }} >20%</div>
-        </div>
-
-        <div className="progress mb-2" style={{ minHeight: "1.5rem" }}>
-          <div className="progress-bar progress-bar-striped progress-bar-animated bg-info" style={{ minWidth: "30%" }} >30%</div>
-        </div>
-
-        <div className="progress mb-2" style={{ minHeight: "1.5rem" }}>
-          <div className="progress-bar progress-bar-striped progress-bar-animated bg-warning" style={{ minWidth: "40%" }} >40%</div>
-        </div>
-
-        <div className="progress mb-2" style={{ minHeight: "1.5rem" }}>
-          <div className="progress-bar bg-danger" style={{ minWidth: "50%" }} >50%</div>
-        </div>
-
-        <div className="progress mb-2" style={{ minHeight: "1.5rem" }}>
-          <div className="progress-bar bg-secondary" style={{ minWidth: "60%" }} >60%</div>
-        </div>
-
-        <div className="progress mb-2" style={{ minHeight: "1.5rem" }}>
-          <div className="progress-bar bg-dark" style={{ minWidth: "70%" }} >70%</div>
-        </div>
-
-        <div className="progress mb-2" style={{ minHeight: "1.5rem" }}>
-          <div className="progress-bar bg-light text-black" style={{ minWidth: "80%" }} >80%</div>
+        .<div className="d-flex justify-content-center align-items-center">
+          <div className="spinner-border spinner-border-sm text-muted"></div>
+          <div className="spinner-border spinner-border-sm text-primary"></div>
+          <div className="spinner-border spinner-border-sm text-success"></div>
+          <div className="spinner-border spinner-border-sm text-info"></div>
+          <div className="spinner-grow spinner-grow-sm text-warning"></div>
+          <div className="spinner-grow spinner-grow-sm text-danger"></div>
+          <div className="spinner-grow spinner-grow-sm text-secondary"></div>
+          <div className="spinner-grow spinner-grow-sm text-dark"></div>
+          <div className="spinner-grow spinner-grow-sm text-light"></div>
         </div>
       </div>
 
-      <div className="container-md pt-3">
-        <h2>Multiple progress bars</h2>
-        <p>Create a stacked progress bar by placing multiple bars into the same div with class=&quot;progress&quot;:</p>
-        <div className="progress">
-          <div className="progress-bar bg-success" role="progressbar" style={{ minWidth: "40%" }} >Free Space</div>
-          <div className="progress-bar bg-warning" role="progressbar" style={{ minWidth: "10%" }} >Warning</div>
-          <div className="progress-bar bg-danger" role="progressbar" style={{ minWidth: "20%" }} >Danger</div>
+      <div className="container-md pt-4">
+
+        <div className="d-flex justify-content-center align-items-center">
+          <button type="button" className="btn btn-primary">
+            <span className="spinner-border spinner-border-sm"></span>
+          </button>
+
+          <button type="button" className="btn btn-info">
+            <span className="spinner-border spinner-border-sm"></span> Loading...
+          </button>
+
+
+          <button type="button" className="btn btn-success" disabled>
+            <span className="spinner-grow spinner-grow-sm"></span> Loading...
+          </button>
+
+
+          <button type="button" className="btn btn-warning" disabled>
+            <span className="spinner-grow spinner-grow-sm"></span> Loading...
+          </button>
         </div>
       </div>
     </>
