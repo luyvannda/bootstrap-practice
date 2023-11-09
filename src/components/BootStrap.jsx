d15a804b3674693029262d8b4eb2e068407f36f0
luyvannda <philips.joe329@gmail.com>
2023-11-09 14:57:15 +0700

    practice basic collapse item

--- src/components/BootStrap.jsx
+++ src/components/BootStrap.jsx
@@ -3,127 +3,26 @@
 export default function Bootstrap() {
   return (
     <>
-      <div className="container pt-3  d-flex justify-content-center">
-        <div className="dropdown">
-          <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-            Dropdown Button
-          </button>
-          <ul className="dropdown-menu" >
-            <li><h5 className="dropdown-header">Header 1</h5></li>
-            <li> <a className="dropdown-item" href="#">Link 1</a></li>
-            <li> <a className="dropdown-item" href="#">Link 2</a></li>
-            <li> <a className="dropdown-item" href="#">Link 3</a></li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <h5 className="dropdown-header">Header 2</h5>
-            </li>
-            <li><a href="#" className="dropdown-item">Another Link</a></li>
-          </ul>
-        </div>
-      </div>
       <div className="container pt-3">
-        <div className="dropdown dropend">
-          <button className="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown">
-            Dropdown Button
-          </button>
-          <ul className="dropdown-menu" >
-            <li><h5 className="dropdown-header">Header 1</h5></li>
-            <li> <a className="dropdown-item" href="#">Link 1</a></li>
-            <li> <a className="dropdown-item" href="#">Link 2</a></li>
-            <li> <a className="dropdown-item" href="#">Link 3</a></li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <h5 className="dropdown-header">Header 2</h5>
-            </li>
-            <li><a href="#" className="dropdown-item">Another Link</a></li>
-          </ul>
-        </div>
-      </div>
 
-      <div className="container-md pt-3 d-flex justify-content-end">
-        <div className="dropdown dropstart">
-          <button className="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown">
-            2nd Dropdown Menu
-          </button>
-          <ul className="dropdown-menu">
-            <li><a href="#" className="dropdown-item">Default Link</a></li>
-            <li><a href="#" className="dropdown-item active">Active Link</a></li>
-            <li><a href="#" className="dropdown-item disabled">Disabled Link</a></li>
-          </ul>
+        <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#show1">
+          Show
+        </button>
+        <div className="collapse" id="show1">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa ea officia omnis voluptate deleniti ipsa facere in facilis ad esse atque eos eaque, eum vero ipsum? Maxime alias iure labore?
         </div>
       </div>
 
       <div className="container-md pt-3">
-        <div className="dropdown dropdown-menu-end">
-          <button className="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown">
-            This wide drop down menu is an example.
-          </button>
-          <ul className="dropdown-menu">
-            <li><a href="#" className="dropdown-item">Default Link</a></li>
-            <li><a href="#" className="dropdown-item active">Active Link</a></li>
-            <li><a href="#" className="dropdown-item disabled">Disabled Link</a></li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="container-md pt-3 ">
-        <div className="dropdown ">
-          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-            Drop down with text
-          </button>
-          <ul className="dropdown-menu">
-            <li><a href="#" className="dropdown-item">Link 1</a></li>
-            <li><a href="#" className="dropdown-item">Link 2</a></li>
-            <li><a href="#" className="dropdown-item">Link 3</a></li>
-            <li><a href="#" className="dropdown-item-text">Text link</a></li>
-            <li><span className="dropdown-item-text">Just text</span></li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="container-md pt-3 fixed-bottom">
-        <div className="dropup dropdown-menu-end">
-          <button className="btn btn-dark dropdown-toggle" type="button" data-bs-toggle="dropdown">
-            The drop-up button.
-          </button>
-          <ul className="dropdown-menu">
-            <li><a href="#" className="dropdown-item">Default Link</a></li>
-            <li><a href="#" className="dropdown-item active">Active Link</a></li>
-            <li><a href="#" className="dropdown-item disabled">Disabled Link</a></li>
-          </ul>
+        <p>For {`<a>`} tag we use href instead of data-bs-target for collapse id</p>
+        <a href="#show2" className="btn btn-info" data-bs-toggle="collapse">Show</a>
+        <div className="collapse show" id="show2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa ea officia omnis voluptate deleniti ipsa facere in facilis ad esse atque eos eaque, eum vero ipsum? Maxime alias iure labore?
         </div>
       </div>
 
       <div className="container-md pt-3">
-        <div className="btn-group">
-          <button className="btn btn-primary">Apple</button>
-          <button className="btn btn-primary">Samsung</button>
-          <div className="btn-group">
-            <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown">Sony</button>
-            <ul className="dropdown-menu">
-              <li><a href="#" className="dropdown-item">PS5</a></li>
-              <li><a href="#" className="dropdown-item">Bravia TV</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div className="container-md pt-3">
-        <div className="btn-group-vertical">
-          <button className="btn btn-primary">Apple</button>
-          <button className="btn btn-primary">Samsung</button>
-          <div className="btn-group">
-            <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown">Sony</button>
-            <ul className="dropdown-menu">
-              <li><a href="#" className="dropdown-item">PS5</a></li>
-              <li><a href="#" className="dropdown-item">Bravia TV</a></li>
-            </ul>
-          </div>
-        </div>
+        <h2>Accordion</h2>
       </div>
     </>
   )
