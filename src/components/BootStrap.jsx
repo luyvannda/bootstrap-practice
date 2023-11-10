c2dd0fc2b330bb232527e3f7d3c324becc208b61
luyvannda <philips.joe329@gmail.com>
2023-11-10 16:23:12 +0700

    fix navbar toggler collapsible item not working on React

--- src/components/BootStrap.jsx
+++ src/components/BootStrap.jsx
@@ -4,26 +4,31 @@ export default function Bootstrap() {
   return (
     <>
       {/* Remove the .navbar-expand-* class to create a navigation bar that will always be vertical: */}
-      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#" >
-            <img src="./avatar1.png" alt="an avatar logo" style={{ width: "40px" }} className="rounded-pill"></img>
-            <span className="navbar-text"> Navbar Text</span>
-          </a>
-
-          <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar"><span className="navbar-toggler-icon"></span></button>
-
-          <div className="collapse navbar-collapse" id="collapsibleNavbar">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item"><a href="#" className="nav-link">Link</a></li>
-              <li className="nav-item"><a href="#" className="nav-link">Link</a></li>
-              <li className="nav-item"><a href="#" className="nav-link">Link</a></li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
-
-
 
     </>
   )
